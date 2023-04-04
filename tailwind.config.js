ca20bf271e9da6da97e3b8479303b685a8b704a7
Yuan Zhang <yu_an12@163.com>
2023-04-04 17:28:01 -0400

    test two

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./**/*.{html,js}"],
+export default {
+    content: [
+        "./index.html",
+        "./**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
     },
